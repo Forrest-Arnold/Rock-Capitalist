14fad3a7f3a595a773b67fd8538c0a7d3982bba7
Forrest-Arnold <forkalarn@gmail.com>
2025-03-07 17:50:42 -0800

    Project assets ready

---
+++ Rock-Capitalist/View/Game_Screen.swift
@@ -0,0 +1,24 @@
+//
+//  Game_Screen.swift
+//  Rock-Capitalist
+//
+//  Created by Forrest Kalani Arnold on 3/7/25.
+//
+
+import SwiftUI
+
+struct Game_Screen: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image("Coal")
+                Image("Copper")
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    Game_Screen()
+}
